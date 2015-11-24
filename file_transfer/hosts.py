e3c5d614d9de5d043f81d18be941d83f23720849
Hadrien Titeux <hadwarez@gmail.com>
2015-11-24 17:57:57 +0100

    added an example config file (in the cnfig_files folder), to be able to reflect on the config objet's structure

--- file_transfer/hosts.py
+++ file_transfer/hosts.py
@@ -14,32 +14,73 @@ class MissingCredentials(FileTransferError):
 class MissingDomain(FileTransferError):
     pass
 
-class HostConnectionStatus(Enum):
-    DISCONNECTED = 1
-    CONNECTION = 2
+class SSHCredentials:
+
+    class CredentialsType(Enum):
+        KEY = 1
+        PASSWORD = 2
+
+    def __init__(self, cred_type, *args):
+        self.cred_type = cred_type
+        try:
+            if cred_type == self.CredentialsType.KEY:
+                self.key_path = args[0]
+            else:
+                self.login, self.password = args[0], args[1]
+        except:
+            MissingCredentials()
+
+class RemoteConnection:
+
+    class Status(Enum):
+        DISCONNECTED = 1
+        CONNECTION = 2
+
+    def __init__(self, credentials, domain):
+        self.status = self.Status.DISCONNECTED
+        self.credentials = credentials
+        self.domain = domain
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+class SFTPConnection(RemoteConnection):
+
+    def __init__(self, credentials, domain):
+        super().__init__(credentials, domain)
+        self.connection = None
+
+class SSHConnection(RemoteConnection):
+
+    def __init__(self, credentials, domain):
+        super().__init__(credentials, domain)
+        self.connection = None
 
 class Host:
 
     def __init__(self, **kwargs):
 
         if kwargs.get("key"):
-            pass
+            self.credentials = SSHCredentials(SSHCredentials.CredentialsType.KEY, kwargs.get("key"))
         elif kwargs.get("login") and kwargs.get("password"):
-            pass
+            self.credentials = SSHCredentials(SSHCredentials.CredentialsType.KEY,
+                                              kwargs.get("login"),
+                                              kwargs.get("password"))
         else:
             raise MissingCredentials()
 
-        self.remote_location = kwargs.getget("domain", "")
+        self.remote_location = kwargs.get("remote_location", "")
 
         try:
             self.domain = kwargs["domain"]
         except KeyError:
             raise MissingDomain()
 
-        self.sftp_connection_status = HostConnectionStatus.DISCONNECTED
-        self.sftp_connection = None
-        self.ssh_connection_status = HostConnectionStatus.DISCONNECTED
-        self.ssh_connection = None
+        self.sftp_conection = SFTPConnection(self.credentials, self.domain)
+        self.ssh_connection = SSHConnection(self.credentials, self.domain)
 
     def sftp_connect(self):
         """Opens a SFTP connection with the remote host"""
